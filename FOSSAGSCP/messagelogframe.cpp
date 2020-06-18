1811f143b4f4c6e6a17956e7991df1b7e2f3c695
Bambofy <rbamfordz@gmail.com>
2020-06-18 14:34:32 +0100

    message log saving to file

--- FOSSAGSCP/messagelogframe.cpp
+++ FOSSAGSCP/messagelogframe.cpp
@@ -11,16 +11,69 @@ MessageLogFrame::MessageLogFrame(QWidget *parent) :
 
     m_messageLogListModel = new QStringListModel(ui->messageLogListView);
     ui->messageLogListView->setModel(m_messageLogListModel);
+    ui->messageLogListView->setAutoScroll(true);
 }
 
 void MessageLogFrame::ReceivedMessageLogged(char* timestamp, char dirBit, char opId, char* payload)
 {
     m_messageLogListModel->insertRow(m_messageLogListModel->rowCount());
     QModelIndex row = m_messageLogListModel->index(m_messageLogListModel->rowCount() - 1, 0);
-    m_messageLogListModel->setData(row, payload);
+
+    std::string loggedMessage;
+    if (m_logTimestamps)
+    {
+        loggedMessage += timestamp;
+    }
+
+    loggedMessage += "(dir=";
+    loggedMessage += std::to_string(dirBit);
+    loggedMessage += ") ";
+
+    loggedMessage += "(op=";
+    loggedMessage += std::to_string(opId);
+    loggedMessage += ") \r\n";
+
+    loggedMessage += payload;
+
+    const char * msgTolog = loggedMessage.c_str();
+    m_messageLogListModel->setData(row, msgTolog);
+
+    ui->messageLogListView->scrollToBottom();
 }
 
 MessageLogFrame::~MessageLogFrame()
 {
     delete ui;
+}
+
+void MessageLogFrame::on_enableTimestampsCheckBox_stateChanged(int arg1)
+{
+    m_logTimestamps = arg1;
+}
+
+void MessageLogFrame::on_saveAsButton_clicked()
+{
+    QStringList log = m_messageLogListModel->stringList();
+
+    QString outputLog = log.join("\r\n");
+    std::string outputLogStr = outputLog.toStdString();
+
+    QFileDialog dialog(this);
+    dialog.setFileMode(QFileDialog::FileMode::AnyFile);
+    dialog.setViewMode(QFileDialog::Detail);
+
+    if (dialog.exec())
+    {
+        QStringList filenames = dialog.selectedFiles();
+
+        for (int i = 0; i < filenames.size(); i++)
+        {
+            QFile file(filenames[i]);
+
+            if (file.open(QIODevice::WriteOnly | QIODevice::Text))
+            {
+                file.write(outputLogStr.c_str());
+            }
+        }
+    }
 }
--- FOSSAGSCP/messagelogframe.h
+++ FOSSAGSCP/messagelogframe.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QStringListModel>
+#include <QFileDialog>
 
 namespace Ui {
 class MessageLogFrame;
@@ -18,9 +19,15 @@ public:
 
 public slots:
     void ReceivedMessageLogged(char* timestamp, char dirBit, char opId, char* payload); // attached to MessageLog.h
+private slots:
+    void on_enableTimestampsCheckBox_stateChanged(int arg1);
+
+    void on_saveAsButton_clicked();
+
 private:
     Ui::MessageLogFrame *ui;
     QStringListModel * m_messageLogListModel;
+    bool m_logTimestamps = false;
 };
 
 #endif // MESSAGELOGFRAME_H
