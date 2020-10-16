d675011872869aaa1a936cb269082e344728ce0b
Bambofy <rbamfordz@gmail.com>
2020-10-16 11:46:15 +0100

    FS2 packet info  message handler

--- Desktop/FOSSA-GroundStationControlPanel/FOSSAGSCP/MessageHandlers/FOSSASAT2_MessageHandler.cpp
+++ Desktop/FOSSA-GroundStationControlPanel/FOSSAGSCP/MessageHandlers/FOSSASAT2_MessageHandler.cpp
@@ -7,43 +7,53 @@
 
 void FOSSASAT2::MessageHandler::Handle(FOSSASAT2::Messages::SystemInfo msg, systeminformationpane* systemInfoPane)
 {
-    systemInfoPane->GetUI()->SystemInformation_MPPT_OutputVoltage_SpinBox->setValue(msg.GetMpptOutputVoltage());
-    systemInfoPane->GetUI()->SystemInformation_MPPT_OutputCurrent_SpinBox->setValue(msg.GetMpptOutputCurrent());
+    Ui::systeminformationpane* ui = systemInfoPane->GetUI();
 
-    systemInfoPane->GetUI()->SystemInformation_UnixTimestamp_LineEdit->setText(QString::number(msg.GetUnixTimestamp()));
+    ui->SystemInformation_MPPT_OutputVoltage_SpinBox->setValue(msg.GetMpptOutputVoltage());
+    ui->SystemInformation_MPPT_OutputCurrent_SpinBox->setValue(msg.GetMpptOutputCurrent());
 
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_TransmissionsEnabled_Enabled_RadioButton->setChecked(msg.IsTransmissionsEnabled());
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_TransmissionsEnabled_Disabled_RadioButton->setChecked(!msg.IsTransmissionsEnabled());
+    ui->SystemInformation_UnixTimestamp_LineEdit->setText(QString::number(msg.GetUnixTimestamp()));
 
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_LowPowerMode_Enabled_RadioButton->setChecked(msg.IsLowPowerModeEnabled());
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_LowPowerMode_Disabled_RadioButton->setChecked(!msg.IsLowPowerModeEnabled());
+    ui->SystemInformation_PowerConfiguration_TransmissionsEnabled_Enabled_RadioButton->setChecked(msg.IsTransmissionsEnabled());
+    ui->SystemInformation_PowerConfiguration_TransmissionsEnabled_Disabled_RadioButton->setChecked(!msg.IsTransmissionsEnabled());
 
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_LowPowerActiveMode_LP_RadioButton->setChecked(msg.IsLowPowerModeActive());
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_LowPowerActiveMode_Sleep_RadioButton->setChecked(msg.IsSleepMode());
+    ui->SystemInformation_PowerConfiguration_LowPowerMode_Enabled_RadioButton->setChecked(msg.IsLowPowerModeEnabled());
+    ui->SystemInformation_PowerConfiguration_LowPowerMode_Disabled_RadioButton->setChecked(!msg.IsLowPowerModeEnabled());
 
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_MPPTTemperatureSwitchEnabled_Enabled_RadioButton->setChecked(msg.GetMpptTemperatureSwitchEnabled());
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_MPPTTemperatureSwitchEnabled_Disabled_RadioButton->setChecked(!msg.GetMpptTemperatureSwitchEnabled());
+    ui->SystemInformation_PowerConfiguration_LowPowerActiveMode_LP_RadioButton->setChecked(msg.IsLowPowerModeActive());
+    ui->SystemInformation_PowerConfiguration_LowPowerActiveMode_Sleep_RadioButton->setChecked(msg.IsSleepMode());
 
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_MPPTKeepAliveEnabled_Enabled_RadioButton->setChecked(msg.GetMpptKeepAliveEnabled());
-    systemInfoPane->GetUI()->SystemInformation_PowerConfiguration_MPPTKeepAliveEnabled_Disabled_RadioButton->setChecked(!msg.GetMpptKeepAliveEnabled());
+    ui->SystemInformation_PowerConfiguration_MPPTTemperatureSwitchEnabled_Enabled_RadioButton->setChecked(msg.GetMpptTemperatureSwitchEnabled());
+    ui->SystemInformation_PowerConfiguration_MPPTTemperatureSwitchEnabled_Disabled_RadioButton->setChecked(!msg.GetMpptTemperatureSwitchEnabled());
 
-    systemInfoPane->GetUI()->SystemInformation_ResetCounter_SpinBox->setValue(msg.GetResetCounter());
+    ui->SystemInformation_PowerConfiguration_MPPTKeepAliveEnabled_Enabled_RadioButton->setChecked(msg.GetMpptKeepAliveEnabled());
+    ui->SystemInformation_PowerConfiguration_MPPTKeepAliveEnabled_Disabled_RadioButton->setChecked(!msg.GetMpptKeepAliveEnabled());
 
-    systemInfoPane->GetUI()->SystemInformation_SolarPanelsPowerInformation_SolarPanelXAVoltage_SpinBox->setValue(msg.GetSolarPanelXaVoltage());
-    systemInfoPane->GetUI()->SystemInformation_SolarPanelsPowerInformation_SolarPanelXBVoltage_SpinBox->setValue(msg.GetSolarPanelXbVoltage());
-    systemInfoPane->GetUI()->SystemInformation_SolarPanelsPowerInformation_SolarPanelZAVoltage_SpinBox->setValue(msg.GetSolarPanelZaVoltage());
-    systemInfoPane->GetUI()->SystemInformation_SolarPanelsPowerInformation_SolarPanelZBVoltage_SpinBox->setValue(msg.GetSolarPanelZbVoltage());
-    systemInfoPane->GetUI()->SystemInformation_SolarPanelsPowerInformation_SolarPanelYVoltage_SpinBox->setValue(msg.GetSolarPanelYVoltage());
+    ui->SystemInformation_ResetCounter_SpinBox->setValue(msg.GetResetCounter());
 
-    systemInfoPane->GetUI()->SystemInformation_TemperatureInformation_BatteryTemperature_DoubleSpinBox->setValue(msg.GetBatteryTemperature());
-    systemInfoPane->GetUI()->SystemInformation_TemperatureInformation_OBCBoardTemperature_DoubleSpinBox->setValue(msg.GetObcBoardTemperature());
+    ui->SystemInformation_SolarPanelsPowerInformation_SolarPanelXAVoltage_SpinBox->setValue(msg.GetSolarPanelXaVoltage());
+    ui->SystemInformation_SolarPanelsPowerInformation_SolarPanelXBVoltage_SpinBox->setValue(msg.GetSolarPanelXbVoltage());
+    ui->SystemInformation_SolarPanelsPowerInformation_SolarPanelZAVoltage_SpinBox->setValue(msg.GetSolarPanelZaVoltage());
+    ui->SystemInformation_SolarPanelsPowerInformation_SolarPanelZBVoltage_SpinBox->setValue(msg.GetSolarPanelZbVoltage());
+    ui->SystemInformation_SolarPanelsPowerInformation_SolarPanelYVoltage_SpinBox->setValue(msg.GetSolarPanelYVoltage());
 
-    systemInfoPane->GetUI()->SystemInformation_FlashInformation_CRCError_SpinBox->setValue(msg.GetFlashSystemInfoPageCrcErrorCounter());
+    ui->SystemInformation_TemperatureInformation_BatteryTemperature_DoubleSpinBox->setValue(msg.GetBatteryTemperature());
+    ui->SystemInformation_TemperatureInformation_OBCBoardTemperature_DoubleSpinBox->setValue(msg.GetObcBoardTemperature());
+
+    ui->SystemInformation_FlashInformation_CRCError_SpinBox->setValue(msg.GetFlashSystemInfoPageCrcErrorCounter());
 }
 
 void FOSSASAT2::MessageHandler::Handle(FOSSASAT2::Messages::PacketInfo msg, systeminformationpane* systemInfoPane)
 {
+    Ui::systeminformationpane* ui = systemInfoPane->GetUI();
 
+    ui->PacketInformation_SNR_SpinBox->setValue(msg.GetSnr());
+    ui->PacketInformation_RSSI_SpinBox->setValue(msg.GetRssi());
+
+    ui->PacketInformation_LoraFrameInformation_NumValidFrames_SpinBox->setValue(msg.GetNumReceivedValidLoraFrames());
+    ui->PacketInformation_LoraFrameInformation_NumInvalidFrames_SpinBox->setValue(msg.GetNumReceivedInvalidLoraFrames());
+    ui->PacketInformation_FSKFrameInformation_NumValidFrames_SpinBox->setValue(msg.GetNumReceivedValidFskFrames());
+    ui->PacketInformation_FSKFrameInformation_NumInvalidFrames_SpinBox->setValue(msg.GetNumReceivedInvalidFskFrames());
 }
 
 void FOSSASAT2::MessageHandler::Handle(FOSSASAT2::Messages::Statistics msg, systeminformationpane* systemInfoPane)
